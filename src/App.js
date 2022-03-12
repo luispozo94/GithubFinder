94bd73e288a3de00e825bf3ac1e2650b94585338
Luis Pozo <lpozo94@gmail.com>
2022-03-11 21:05:16 -0500

    created a pages folder to add routes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import Footer from './components/Layout/Footer.js';
 import Navbar from './components/Layout/Navbar.js';
 
-function App() {
+const App = () => {
 	return (
 		<Router>
 			<div className="flex flex-col justify-between h-screen">
 				<Navbar />
-				<main>Content</main>
+				<main className="container mx-auto px-3 pb-12">Content</main>
+				<Footer />
 			</div>
 		</Router>
 	);
-}
+};
 
 export default App;
--- src/components/Layout/Navbar.js
+++ src/components/Layout/Navbar.js
@@ -11,12 +11,15 @@ const Navbar = ({ title }) => {
 					<Link to="/" className="text-lg font-bold align-middle">
 						{title}
 					</Link>
-        </div>
-        
+				</div>
+
 				<div className="flex-1 px-2 mx-2">
 					<div className="flex justify-end">
 						<Link to="/" className="btn btn-ghost btn-sm rounded-btn">
 							Home
+						</Link>
+						<Link to="/about" className="btn btn-ghost btn-sm rounded-btn">
+							About
 						</Link>
 					</div>
 				</div>
